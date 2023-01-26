d15b68cb23618ad601c9a796236edcd1179fe39d
nikunj8829 <123400214+nikunj8829@users.noreply.github.com>
2023-01-26 05:31:58 +0530

    Add files via upload

---
+++ Module_1/ModeEx.kt
@@ -0,0 +1,11 @@
+fun main()
+{
+    var a= 4
+    var b= 2
+
+    var c=a%b
+    println(c)
+
+    var d=a/b
+    println(d)
+}
---
+++ Module_1/OddEvenEx.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+    var number = 11
+
+    if (number%2==0)
+    {
+        println("Number is Even")
+    }
+    else
+    {
+        println("Number is Odd")
+    }
+}
